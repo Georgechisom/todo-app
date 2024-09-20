a5d3ca5749ae8155b3849515eeea331bd67581c0
George winsome <georgechipaul@gmail.com>
2024-09-20 19:26:15 +0100

    all updated

--- src/App.js
+++ src/App.js
@@ -3,20 +3,109 @@ import Todo from "./components/Todo";
 import './index.js'
 import Form from './components/Form.js';
 import FilterButton from './components/FilterButton.js';
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
+import {nanoid} from "nanoid";
+
+
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 
 function App(props) {
-
-  function addTask(name) {
-    alert(name);
-  };
   
   const [tasks, setTasks] = useState(props.tasks);
 
-  const taskList = tasks?.map((task) => (
-    <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} />
+  const [filter, setFilter] = useState("All");
+
+  function addTask(name) {
+    const newTask = {id: `todo-${nanoid()}`, name, completed: false};
+    setTasks([...tasks, newTask]);
+  };
+
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  };
+
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // Copy the task and update its name
+        return { ...task, name: newName };
+      }
+      // Return the original task if it's not the edited task
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
+    <Todo id={task.id} 
+          name={task.name} 
+          completed={task.completed} 
+          key={task.id}
+          toggleTaskCompleted={toggleTaskCompleted}
+          editTask={editTask}
+          deleteTask={deleteTask}
+    />
   ));
+
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
+  
+  
+
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
+  const listHeadingRef = useRef(null);
+
+  const prevTaskLength = usePrevious(tasks.length);
+
+  useEffect(() => {
+    if (tasks.length < prevTaskLength) {
+      listHeadingRef.current.focus();
+    }
+  }, [tasks.length, prevTaskLength]);
+  
+
 
 
   return (
@@ -24,12 +113,10 @@ function App(props) {
       <h1>TodoMatic</h1>
       <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
         
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading" tabIndex="-1" ref={listHeadingRef} >{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
