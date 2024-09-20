c4fb602baa787e90a16535ec5b2758395df81f0e
George winsome <georgechipaul@gmail.com>
2024-09-20 19:32:08 +0100

    all updated

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const DATA = [
-  { id: "task-0", name: "Eat", completed: true },
-  { id: "task-1", name: "Sleep", completed: false },
-  { id: "task-2", name: "Repeat", completed: false },
+  { id: "task-0", name: "Morning Exercise", completed: true },
+  { id: "task-1", name: "Cooking Breakfast", completed: false },
+  { id: "task-2", name: "Studying React Lessons", completed: false },
 ];
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
